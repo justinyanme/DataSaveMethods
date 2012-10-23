2414dea437730db0a778b79c82fd94a5e8f8648c
Justin Yim <justinyan@Justins-MacBook-Air.local>
2012-10-23 17:32:32 +0800

    Initial Commit

---
+++ DataSaveMethods/WSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WSAppDelegate.h
+//  DataSaveMethods
+//
+//  Created by Justin Yim on 10/23/12.
+//  Copyright (c) 2012 WhisperStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DataSaveMethods/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DataSaveMethods
+//
+//  Created by Justin Yim on 10/23/12.
+//  Copyright (c) 2012 WhisperStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WSAppDelegate class]));
+    }
+}
